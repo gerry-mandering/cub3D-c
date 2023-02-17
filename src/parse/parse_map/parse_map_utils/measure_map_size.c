87a09d31b1542feb69601bbfcfe271501a3be90a
gerry-mandering <kimmen1119@gmail.com>
2023-02-17 21:33:45 +0900

    Fix: fix map validation

--- src/parse/parse_map/parse_map_utils/measure_map_size.c
+++ src/parse/parse_map/parse_map_utils/measure_map_size.c
@@ -6,7 +6,7 @@
 /*   By: minseok2 <minseok2@student.42seoul.kr      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/14 11:14:55 by minseok2          #+#    #+#             */
-/*   Updated: 2023/02/16 20:42:17 by minseok2         ###   ########.fr       */
+/*   Updated: 2023/02/17 21:15:08 by minseok2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,4 +26,6 @@ void	measure_map_size(int *map_width, int *map_height, t_list *list_map)
 		(*map_height)++;
 		list_map = list_map->next;
 	}
+	if (*map_height < 3 || *map_width < 3)
+		error_handler(INVALID_MAP_SIZE);
 }
